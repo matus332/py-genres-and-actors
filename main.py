be62f271d2cdf0cd8c972bff6581eee08b9991ed
Denys Matusevych <denytop3@gmail.com>
2026-04-20 12:13:10 +0300

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,64 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(
+        name="Western",
+
+    )
+    Genre.objects.create(
+        name="Action",
+
+    )
+    Genre.objects.create(
+        name="Dramma",
+
+    )
+    Actor.objects.create(
+        first_name="George",
+        last_name="Klooney",
+    )
+    Actor.objects.create(
+        first_name="Kianu",
+        last_name="Reaves",
+    )
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Keegan",
+    )
+    Actor.objects.create(
+        first_name="Will",
+        last_name="Smith",
+    )
+    Actor.objects.create(
+        first_name="Jaden",
+        last_name="Smith",
+    )
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Johansson",
+    )
+    Genre.objects.filter(
+        name="Dramma"
+    ).update(name="Drama")
+    Actor.objects.filter(
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).update(last_name="Reeves", first_name="Keanu")
+    Genre.objects.filter(
+        name="Action"
+    ).delete()
+    Actor.objects.filter(
+        first_name="Scarlett"
+    ).delete()
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
